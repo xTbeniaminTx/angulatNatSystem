765f027016c09e101f2ea158521998184657f02f
xTbeniaminTx <beniamin.tolan@outlook.com>
2022-03-07 14:10:57 +0100

    interface user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,98 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {
+  NgModule
+} from '@angular/core';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  FormsModule
+} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { TestBindingComponent } from './test-binding/test-binding.component';
+import {
+  AppComponent
+} from './app.component';
+import {
+  NavbarComponent
+} from './navbar/navbar.component';
+import {
+  TestBindingComponent
+} from './test-binding/test-binding.component';
 
-import { PanelModule } from 'primeng/panel';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { HighlightDirective } from './directives/highlight.directive';
-import { TimerDirective } from './directives/timer.directive';
-import { CbPipe } from './pipes/cb.pipe';
-import { OrganeComponent } from './organe/organe.component';
-import { AddComponent } from './add/add.component';
-import { PanierComponent } from './panier/panier.component';
-import { PanierService } from './services/panier.service';
-import { AuthComponent } from './auth/auth.component';
-import { ExamplesComponent } from './examples/examples.component';
-import { ExercisesComponent } from './exercises/exercises.component';
-import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './error/error.component';
-import { AuthGuardService } from './services/auth-guard.service';
+import {
+  PanelModule
+} from 'primeng/panel';
+import {
+  BrowserAnimationsModule
+} from '@angular/platform-browser/animations';
+import {
+  HighlightDirective
+} from './directives/highlight.directive';
+import {
+  TimerDirective
+} from './directives/timer.directive';
+import {
+  CbPipe
+} from './pipes/cb.pipe';
+import {
+  OrganeComponent
+} from './organe/organe.component';
+import {
+  AddComponent
+} from './add/add.component';
+import {
+  PanierComponent
+} from './panier/panier.component';
+import {
+  PanierService
+} from './services/panier.service';
+import {
+  AuthComponent
+} from './auth/auth.component';
+import {
+  ExamplesComponent
+} from './examples/examples.component';
+import {
+  ExercisesComponent
+} from './exercises/exercises.component';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import {
+  ErrorComponent
+} from './error/error.component';
+import {
+  AuthGuardService
+} from './services/auth-guard.service';
+import {
+  ReactiveFormsModule
+} from '@angular/forms'
 
-const appRoutes: Routes = [
-  {path: 'examples', component: ExamplesComponent},
-  {path: 'exercises', canActivate: [AuthGuardService], component: ExercisesComponent},
-  {path: 'auth', component: AuthComponent},
-  {path: '', component: ExamplesComponent},
-  {path: 'not-found', component: ErrorComponent},
-  {path: '**', redirectTo: 'not-found'}
+const appRoutes: Routes = [{
+    path: 'examples',
+    component: ExamplesComponent
+  },
+  {
+    path: 'exercises',
+    canActivate: [AuthGuardService],
+    component: ExercisesComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: '',
+    component: ExamplesComponent
+  },
+  {
+    path: 'not-found',
+    component: ErrorComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
 ]
 
 @NgModule({
@@ -46,17 +110,18 @@ const appRoutes: Routes = [
     ExamplesComponent,
     ExercisesComponent,
     ErrorComponent
-   
+
   ],
   imports: [
     BrowserModule,
     PanelModule,
     BrowserAnimationsModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule
 
   ],
   providers: [PanierService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/test-binding/test-binding.component.ts
+++ src/app/test-binding/test-binding.component.ts
@@ -1,67 +1,214 @@
-import {Component, OnInit} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
+import {
+  BehaviorSubject,
+  from,
+  interval,
+  Observable,
+  Observer,
+  Subject,
+  Subscription
+} from 'rxjs';
+import {
+  map,
+  filter
+} from 'rxjs/operators';
 
-@Component(
-    {selector: 'app-test-binding', templateUrl: './test-binding.component.html', styleUrls: ['./test-binding.component.scss']}
-)
-export class TestBindingComponent implements OnInit {
-    nom: string;
-    inputType = "date";
-    compteur: number;
-    content = "";
-    sexe = "F";
-    color = "red";
-    monNom = "";
-    articles: any[] = [
-        {
-            "reference": "pomme",
-            "prix": 1.5
-        }, {
-            "reference": "poire",
-            "prix": 1.2
-        }, {
-            "reference": "bannanas",
-            "prix": 1.9
-        }
-    ];
-    organe:string;
-    organes:string[]=[];
-   
-    myDate: Date = new Date();
-    lastUpdateAsync = new Promise((resolve, reject) => {
-        const date = new Date();
-        setTimeout(() => {
-            resolve(date);
-        }, 2000);
+
+interface User {
+  name: string,
+    age: number,
+    email: string
+}
+
+@Component({
+  selector: 'app-test-binding',
+  templateUrl: './test-binding.component.html',
+  styleUrls: ['./test-binding.component.scss']
+})
+export class TestBindingComponent implements OnInit, OnDestroy {
+    myForm: FormGroup;
+  nom: string;
+  inputType = "date";
+  compteur: number;
+  content = "";
+  sexe = "F";
+  color = "red";
+  monNom = "";
+  articles: any[] = [{
+    "reference": "pomme",
+    "prix": 1.5
+  }, {
+    "reference": "poire",
+    "prix": 1.2
+  }, {
+    "reference": "bannanas",
+    "prix": 1.9
+  }];
+  organe: string;
+  organes: string[] = [];
+
+  obs$: Observable < any > ;
+  counter$: Observable < any > ;
+  sub: Subscription;
+  subCounter: Subscription;
+
+  sub2: Subscription;
+
+  counter: number;
+
+  subj = new Subject < number > ();
+  subj2 = new BehaviorSubject < number > (0);
+
+  subjectPipe = new BehaviorSubject < User > ({
+    name: 'jean',
+    age: 28,
+    email: 'jean@gmail.com'
+  });
+
+  sub1 = this.subjectPipe.subscribe((x: User) => {
+    console.log('[sub1] : ', x);
+  })
+
+
+
+  myDate: Date = new Date();
+  lastUpdateAsync = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }, 2000);
+  });
+  name: Promise < string > ;
+
+  constructor() {
+    this.compteur = 0;
+    setTimeout(() => {
+      this.inputType = "number";
+      this.nom = "Angular";
+    }, 10000);
+  }
+
+  ngOnInit(): void {
+
+    this.myForm = new FormGroup({
+        username: new FormControl(''),
+        email: new FormControl(''),
+        password: new FormControl('')
     });
-    name: Promise<string>;
 
-    constructor() {
-        this.compteur = 0;
-        setTimeout(() => {
-            this.inputType = "number";
-            this.nom = "Angular";
-        }, 10000);
-    }
+    console.log("myForm:", this.myForm)
 
-    ngOnInit(): void {
-        this.name = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve('Paul');
-            }, 3000)
-        })
-    }
 
-    public afficheMomentActuel(): string {
-        let auj = new Date();
-        return auj.toLocaleDateString('fr-FR') + " " + auj.toLocaleTimeString();
-    }
+    const sub2 = this.subjectPipe.pipe(
+      map((user: User) => {
+        return user.email;
+      })
+    ).subscribe((email: string) => {
+      console.log('exemple utilisation de map : ', email);
+    })
 
-    public incrementeCompteur() {
-        this.compteur++;
-    }
+    const sub3 = this.subjectPipe.pipe(
+      filter((user: User) => {
+        return true; //essayer avec return false  
+      }),
+      map((user: User) => {
+        return user.email;
+      })
+    ).subscribe((email: string) => {
+      console.log('exemple utilisation de filter : ', email);
+    })
 
-    deleteOrgane(organe) {
-        this.organes.splice(this.organe.indexOf(organe), 1);
-    }
+    const Array$ = from([1, 2, 3, 4, 5]);
+    const resultFilter = Array$.pipe(
+      filter(x => x < 4)
+    );
+    console.log("filter",resultFilter); // donne [1, 2, 3]
+
+
+
+
+    this.name = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Paul');
+      }, 3000)
+    })
+
+
+    this.obs$ = new Observable((observer: Observer < any > ) => {
+      observer.next(2);
+      observer.next(3);
+      setTimeout(() => {
+        observer.next(45);
+      }, 2000)
+      //   observer.error('Message d erreur');
+      observer.next(4);
+      observer.complete();
+    });
+
+    this.sub = this.obs$.subscribe({
+      next: x => console.log('[next] :', x),
+      complete: () => console.log('[complete]'),
+      error: x => console.log('[error] :', x)
+    });
+
+    this.sub2 = this.obs$.subscribe(
+      x => console.log('[sub2 next] :', x),
+      x => console.log('[sub2 error] :', x)
+    );
+
+    const s1 = this.subj.subscribe((x: number) => {
+      console.log('[s1]:', x);
+    })
+
+    const s2 = this.subj.subscribe((x: number) => {
+      console.log('[s2]:', x);
+    })
+
+    this.subj.next(2);
+
+
+    const s3 = this.subj2.subscribe((x: number) => {
+      console.log('[s3] : ', x);
+    })
+
+    const s4 = this.subj2.subscribe((x: number) => {
+      console.log('[s4] : ', x);
+    })
+
+    this.subj2.next(2021);
+
+    this.counter$ = interval(1000);
+    this.subCounter = this.counter$.subscribe({
+      next: x => this.counter = x,
+      complete: () => console.log('[complete]'),
+      error: x => console.log('[error] :', x)
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+    this.sub2.unsubscribe();
+    this.subCounter.unsubscribe();
+
+  }
+
+  public afficheMomentActuel(): string {
+    let auj = new Date();
+    return auj.toLocaleDateString('fr-FR') + " " + auj.toLocaleTimeString();
+  }
+
+  public incrementeCompteur() {
+    this.compteur++;
+  }
+
+  deleteOrgane(organe) {
+    this.organes.splice(this.organe.indexOf(organe), 1);
+  }
 
 }
