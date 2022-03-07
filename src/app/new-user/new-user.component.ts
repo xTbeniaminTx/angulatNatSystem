89f25181a1feb4819bc5f19379e54d42f982033b
xTbeniaminTx <beniamin.tolan@outlook.com>
2022-03-07 17:18:51 +0100

    user list

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -6,7 +6,12 @@ import {
   FormControl,
   FormGroup
 } from '@angular/forms';
-
+import { from } from 'rxjs';
+import {
+  User
+} from '../models/User.model';
+import { UserService } from '../services/user.service';
+  
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
@@ -14,11 +19,11 @@ import {
 })
 export class NewUserComponent implements OnInit {
 
- userForm: FormGroup;
- drinks: string[] = ['late', 'coke', 'lait'];
+  userForm: FormGroup;
+  drinks: string[] = ['late', 'coke', 'lait'];
 
-  constructor() {
-    
+  constructor(private userService: UserService) {
+
   }
 
   ngOnInit(): void {
@@ -32,6 +37,21 @@ export class NewUserComponent implements OnInit {
       email: new FormControl(''),
       drinkPreference: new FormControl(this.drinks)
     })
+  }
+
+  onSubmitForm() {
+    const formValue = this.userForm.value;
+    const newUser = new User(
+      formValue['firstName'],
+      formValue['lastName'],
+      formValue['email'],
+      formValue['drinkPreference'],
+      formValue['hobbies'] ? formValue['hobbies'] : []
+    );
+
+    this.userService.addUser(newUser);
+    console.log(newUser);
+
   }
 
 }
