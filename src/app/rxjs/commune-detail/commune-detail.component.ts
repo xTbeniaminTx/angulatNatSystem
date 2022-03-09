28d7a314c939579c30bf7236809150bfb8a7e3aa
xTbeniaminTx <beniamin.tolan@outlook.com>
2022-03-09 12:31:02 +0100

    details commune

--- src/app/rxjs/commune-detail/commune-detail.component.ts
+++ src/app/rxjs/commune-detail/commune-detail.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {delay, filter} from 'rxjs/operators';
+import {RxjsService} from '../services/rxjs.service';
+import {CommuneModel} from '../model/commune';
 
 @Component({
   selector: 'app-commune-detail',
   templateUrl: './commune-detail.component.html',
   styleUrls: ['./commune-detail.component.scss']
 })
-export class CommuneDetailComponent implements OnInit {
+export class CommuneDetailComponent implements OnInit, OnDestroy {
+  sub: Subscription = new Subscription();
+  selectedCommune: CommuneModel;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    // tslint:disable-next-line:variable-name
+    private _activatedRoute: ActivatedRoute,
+    // tslint:disable-next-line:variable-name
+    private _rxjsService: RxjsService
+  ) {
   }
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  // @ts-ignore
+  ngOnInit(): void {
+    // @ts-ignore
+    this.sub.add(
+      this._activatedRoute.paramMap.pipe(
+        delay(2000),
+        filter((param: ParamMap) => ![null, undefined].includes(param))
+      ).subscribe((param: ParamMap) => {
+          console.log(param);
+          const codeCommune = param.get('code');
+          const codeDepartement = param.get('code').substr(0, 2);
+
+          this._rxjsService.getAllCommuneByDept(codeDepartement).subscribe(
+            {
+              next: (communes: CommuneModel[]) => {
+                this.selectedCommune =
+                  communes.find((comm: CommuneModel) => comm.code === codeCommune);
+              }
+            }
+          );
+        }
+      )
+    );
+  }
 }
--- src/app/rxjs/services/rxjs.service.ts
+++ src/app/rxjs/services/rxjs.service.ts
@@ -33,4 +33,5 @@ getAllCommuneByDept(code: string): Observable<CommuneModel[]> {
 
 
 
+
 }
